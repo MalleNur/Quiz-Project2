cdea123c3be32a7e0f18eadf69c02385c17e5fdd
MalleNur <maljunnur92@gmail.com>
2022-06-25 02:17:44 +0000

    style website to a mobile format

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple
+
+
+
+async function loadQuestion(){
+    const APIUrl = 'https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple';
+    const result = 
+}
