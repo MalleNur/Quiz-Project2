3e463353295012a6549a40660589f2bf6b460354
MalleNur <maljunnur92@gmail.com>
2022-06-27 12:07:36 +0000

    js - setting code for when the game know its a correct or incorrect answer

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ async function loadQuestion(){
     const APIUrl = 'https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple';
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
-    console.log(data);
+    // console.log(data.results[0]);
+    showQuestion(data.results[0]);
+}
+
+function showQuestion(data){
+    let correctAnswer = data.correct_answer;
+    let incorrectAnswer = data.incorrect_answers;
+    let optionsList = incorrectAnswer;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1))))
 }
 
 loadQuestion();
